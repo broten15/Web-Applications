133d04d62d56aae8f34060bc7c4d46c0e2710820
broten15 <66185640+broten15@users.noreply.github.com>
2020-06-18 16:56:19 -0500

    dir change

--- learning_log/11_env/Scripts/activate.bat
+++ learning_log/11_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sbhoc\Documents\GitHub\Web-Applications\learning_log\11_env
+set VIRTUAL_ENV=C:\Users\sbhoc_000\Documents\GitHub\Web-Applications\learning_log\11_env
 
 if not defined PROMPT set PROMPT=$P$G
 
